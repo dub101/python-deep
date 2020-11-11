06f99bc44f56fb19e4e7bba0515fb4b5082e707b
David Uribe <daviduribe@seven4n.com>
2020-11-11 14:57:27 -0500

    Retrained the image classifier using data augmentation

--- python_deep/ch05ex02.py
+++ python_deep/ch05ex02.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
 from keras import layers
 from keras import models
 from keras import optimizers
@@ -5,8 +7,19 @@ from keras.preprocessing.image import ImageDataGenerator
 
 
 def main():
+    
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    for device in gpu_devices:
+        tf.config.experimental.set_memory_growth(device, True)
 
-    train_datagen = ImageDataGenerator(rescale=1.0/255)
+    train_datagen = ImageDataGenerator(rescale=1.0/255,
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
     validation_datagen = ImageDataGenerator(rescale=1.0/255)
 
     train_generator = train_datagen.flow_from_directory(
@@ -31,6 +44,7 @@ def main():
     model.add(layers.Conv2D(128, (3, 3), activation="relu"))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(512, activation="relu"))
     model.add(layers.Dense(1, activation="sigmoid"))
     model.summary()
@@ -42,12 +56,32 @@ def main():
     history = model.fit_generator(
             train_generator,
             steps_per_epoch=100,
-            epochs=30,
+            epochs=100,
             validation_data=validation_generator,
             validation_steps=50)
 
     model.save("cat_dogs_small_1.h5")
-    
+
+    acc = history.history["acc"]
+    val_acc = history.history["val_acc"]
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, "bo", label="Training acc")
+    plt.plot(epochs, val_acc, "b", label="Validation acc")
+    plt.title("Training and validation accuracy")
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, "bo", label="Training loss")
+    plt.plot(epochs, val_loss, "b", label="Validation loss")
+    plt.title("Training and validation loss")
+    plt.legend()
+
+    plt.show()
     
 
 if __name__ == "__main__":
